1c0b66051e4639f35626f2ae9f85c626ba596969
johnryanmal <johnryanmal@gmail.com>
2023-05-12 15:48:12 -0700

    make color picker

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+import ColorPicker from './components/ColorPicker.vue'
 </script>
 
 <template>
+  <ColorPicker />
 </template>
 
 <style scoped>
---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const color = ref('#000000')
+function onInput(event) {
+	const value = event.target.value
+	color.value = value
+}
+</script>
+
+<template>
+	<p>{{ color }}</p>
+	<input type="color" :value="color" @input="onInput"/>
+</template>
