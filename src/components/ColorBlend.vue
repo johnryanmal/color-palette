87a0f187fd00b2df232a1a0bd8e0bb5999b55f2e
johnryanmal <johnryanmal@gmail.com>
2023-05-26 15:00:05 -0700

    generate color ramp from color blend

--- src/components/ColorBlend.vue
+++ src/components/ColorBlend.vue
@@ -1,27 +1,38 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import ColorBand from './ColorBand.vue'
 import ColorSlot from './ColorSlot.vue'
+import ColorSwatch from './ColorSwatch.vue'
 import chroma from 'chroma-js'
 
 const begin = ref(null)
 const end = ref(null)
+const blend = computed(() =>
+  chroma.valid(begin.value?.color) && chroma.valid(end.value?.color)
+    ? chroma.scale([begin.value.color, end.value.color])
+    : (x) => `hsl(0 0% ${x*100}%)`
+)
+const steps = ref(10)
+
+function percentage(min, max, x) {
+  return (x-min)/(max-min) || 0
+}
 </script>
 
 <template>
   <div>
-    <ColorBand
-      v-if="begin && end"
-      v-bind="
-        chroma.valid(begin.color) && chroma.valid(end.color)
-          ? { func: chroma.scale([begin.color, end.color]) }
-          : {}
-      "
-    />
     <div class="full-row">
       <ColorSlot ref="begin"/>
+      <p>Colors: <input type="number" v-model="steps"/></p>
       <ColorSlot ref="end"/>
     </div>
+    <ColorBand :func="blend"/>
+    <hr/>
+    <span>
+      <ColorSwatch v-if="steps > 0" v-for="step in steps"
+        :color="blend(percentage(1, steps, step))"
+      />
+    </span>
   </div>
 </template>
 
